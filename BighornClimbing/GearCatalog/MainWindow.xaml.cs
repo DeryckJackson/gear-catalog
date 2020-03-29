9f5fa416be38223ed0683dfb6c7d6187f2c510e3
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-03-29 10:42:47 -0700

    Added Gear.cs, Database.cs
    Added Gear and Database Class.
    Can now Add things to the Database through the window.

--- BighornClimbing/GearCatalog/MainWindow.xaml.cs
+++ BighornClimbing/GearCatalog/MainWindow.xaml.cs
@@ -20,15 +20,47 @@ namespace GearCatalog
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Database Db;
+
         public MainWindow()
         {
             InitializeComponent();
+            Db = new Database();
+            Db.Connect();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GearList.Items.Add(GearInputTextBox.Text);
-            GearInputTextBox.Text = "";
+            GearList.Items.Add(GearNameTextBox.Text);
+
+            Gear NewGear = new Gear();
+
+            NewGear.Name = GearNameTextBox.Text;
+            NewGear.CategoryId = Int32.Parse(CategoryIdTextBox.Text);
+            NewGear.Description = GearDescriptionTextBox.Text;
+            NewGear.Brand = GearBrandTextBox.Text;
+            NewGear.WeightGrams = Int32.Parse(WeightGramsTextBox.Text);
+            NewGear.LengthMM = Int32.Parse(LengthTextBox.Text);
+            NewGear.WidthMM = Int32.Parse(WidthTextBox.Text);
+            NewGear.DepthMM = Int32.Parse(DepthTextBox.Text);
+            NewGear.Locking = 0;
+
+            if (LockingComboBox.SelectedIndex == 0)
+            {
+                NewGear.Locking = 1;
+            }
+
+            Db.InsertGear(NewGear);
+
+            GearNameTextBox.Text = "";
+            CategoryIdTextBox.Text = "";
+            GearDescriptionTextBox.Text = "";
+            GearBrandTextBox.Text = "";
+            WeightGramsTextBox.Text = "";
+            LengthTextBox.Text = "";
+            WidthTextBox.Text = "";
+            DepthTextBox.Text = "";
+            LockingComboBox.SelectedIndex = 1;
         }
     }
 }
