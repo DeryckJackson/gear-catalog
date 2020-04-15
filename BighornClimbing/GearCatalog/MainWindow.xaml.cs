4ce4f7e72617308d368506b9726ef078a18f07be
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-04-15 08:14:18 -0700

    Update MainWindow.xaml.cs
    Added comment for numeric validation expression.

--- BighornClimbing/GearCatalog/MainWindow.xaml.cs
+++ BighornClimbing/GearCatalog/MainWindow.xaml.cs
@@ -80,6 +80,9 @@ namespace GearCatalog
             }
         }
 
+        /* Uses a Text Composition event to capture user input and pass it to a regex that checks if the input is valid.
+        If the input doesn't match the regex expression, it passes the input to the input field. */
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
